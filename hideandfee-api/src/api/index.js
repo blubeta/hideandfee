67b5bcb4ac635a74982fc384d26ad8b9f7b90a2a
Antonio M <antonio@blubeta.com>
2018-01-20 16:47:57 -0500

    Refactors to single endpoint

--- hideandfee-api/src/api/index.js
+++ hideandfee-api/src/api/index.js
@@ -7,36 +7,51 @@ import bittrexAPI from "../lib/bittrexAPI";
 export default ({ config, db }) => {
   let api = Router();
 
-  // mount the facets resource
-  api.use("/facets", facets({ config, db }));
+  let coinbaseConversion = (fromCoin, toCoin) => {
+    return new Promise((resolve, reject) => {
+      coinbaseAPI
+        .getExchangeRates(fromCoin)
+        .then(resp => {
+          resolve({ version, resp });
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
+  };
+
+  let bittrexConversion = (fromCoin, toCoin) => {
+    return new Promise((resolve, reject) => {
+      bittrexAPI
+        .getExchangeRates(fromCoin, toCoin)
+        .then(resp => {
+          let buy = resp.data.result.buy[0].Rate;
+          let sell = resp.data.result.sell[0].Rate;
+          resolve({ buy, sell });
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
+  };
 
   // perhaps expose some API metadata at the root
   api.get("/", (req, res) => {
-    let coin = req.query.coin.toUpperCase();
-    console.log(coin);
-    coinbaseAPI
-      .getExchangeRates(coin)
-      .then(resp => {
-        res.json({ version, resp });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-
-  api.get("/coinexchange", (req, res) => {
     let fromCoin = req.query.fromCoin.toUpperCase();
     let toCoin = req.query.toCoin.toUpperCase();
-    bittrexAPI
-      .getExchangeRates(fromCoin, toCoin)
-      .then(resp => {
-        let buy = resp.data.result.buy[0].Rate;
-        let sell = resp.data.result.sell[0].Rate;
-        res.json({ buy, sell });
-      })
-      .catch(err => {
-        console.log(err);
+
+    if (
+      (fromCoin == "USD" && toCoin == "BTC") ||
+      (fromCoin == "BTC" && toCoin == "USD")
+    ) {
+      coinbaseConversion(fromCoin, toCoin).then(rate => {
+        res.json(rate);
       });
+    } else {
+      bittrexConversion(fromCoin, toCoin).then(rate => {
+        res.json(rate);
+      });
+    }
   });
 
   return api;
