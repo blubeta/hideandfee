acc293b43c4539fb1537a33ba33fe6e4c710354a
Joseph Engelmajer <joseph.engelmajer@gmail.com>
2018-01-20 17:28:16 -0500

    adds results component

---
+++ hideandfee-app/src/components/Results.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Results extends Component {
+  render(){
+    return(
+      <div>
+        You're Rich!
+      </div>
+    )
+  }
+}
+
+export default Results;
