c18773438f217fbc835546916b9c3922af45d26a
Antonio M <antonio@blubeta.com>
2018-01-20 11:23:33 -0500

    Adds draft structure for exchange fee pair lists

--- src/utils/Rates.js
+++ src/utils/Rates.js
@@ -1,12 +1,5 @@
-const rates = {
-  USD: {
-    BTC: 15.0
-  },
-  BTC: {
-    USD: 0.001,
-    ETH: 0.0001,
-    XRP: 0.2
-  }
-};
+import ExchangeCoinLists from "ExchangeCoinLists";
+
+let rates = {};
 
 export default rates;
