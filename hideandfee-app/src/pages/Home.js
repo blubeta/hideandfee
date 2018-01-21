3b0a0ab11ee7c74371227639d0fc982e347f2cd1
Bryan Padron <Bpadron2000@gmail.com>
2018-01-21 01:38:56 -0500

    adds particles

--- hideandfee-app/src/pages/Home.js
+++ hideandfee-app/src/pages/Home.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Particles from 'react-particles-js'
+import ParticleSettings from '../assets/particleSettings'
 
 class Home extends Component {
   render() {
     return (
       <div className="flex flex-col">
+      <Particles
+      params={
+        ParticleSettings
+      }
+        canvasClassName="particles"
+      />
       <div className="hf-icon"></div>
         <div className="card h-24">
           <span className="text-4xl home-title"> Hide&Fee </span>
