acedb8e865cce9f2f63bd062407aa9f7d05e404f
Joseph Engelmajer <joseph.engelmajer@gmail.com>
2018-01-21 02:13:26 -0500

    results and seech

--- hideandfee-app/src/components/Results.js
+++ hideandfee-app/src/components/Results.js
@@ -9,20 +9,22 @@ class Results extends Component {
 
   render(){
     return(
-      <div>
-      <div className="result-header">
-        <span> Results </span>
-      </div>
+      <div className="flex flex-col items-center">
+        <span className="result-header"> Results </span>
+        <div className="result-row-section px-8" style={{ width: '75%' }}>
+          <span className="text-left"> Trade Details </span>
+          <span className="text-right"> Fees </span>
+        </div>
         { this.props.steps && this.props.steps.map((step) => {
             return (
               <div className="result-row">
                 <div className="result-row-section">
-                  <span> { step.title } </span>
-                  <span> { step.fiatPrice.toFixed(2) } </span>
+                  <span className="text-left"> { step.title } </span>
+                  <span className="text-right"> { `$${step.fiatPrice.toFixed(2)}` } </span>
                 </div>
                 <div className="result-row-section">
-                  <span> { step.description } </span>
-                  <span> { step.btcPrice.toFixed(4) } </span>
+                  <span className="text-sm text-left"> { step.description } </span>
+                  <span className="text-sm text-right"> { `${step.btcPrice.toFixed(4)} BTC` } </span>
                 </div>
               </div>
             )
