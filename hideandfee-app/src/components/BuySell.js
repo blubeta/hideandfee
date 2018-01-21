5dab11e0a1f67ace0fcd67930606b9687d591b78
Joseph Engelmajer <joseph.engelmajer@gmail.com>
2018-01-20 22:28:44 -0500

    summary bar and shit

--- hideandfee-app/src/components/BuySell.js
+++ hideandfee-app/src/components/BuySell.js
@@ -8,8 +8,12 @@ class BuySell extends Component {
     this.state = {
       transaction: null,
       baseCurrency: null,
+      baseConversion: 0,
+      baseConversionUSD: 0,
       exchangeCurrency: null,
       exchangeAmount: 0,
+      exchangeConversionUSD: 0,
+      exchangeConversion: 0,
       isEditing: true,
     }
   }
@@ -19,63 +23,85 @@ class BuySell extends Component {
     const buyOrSell = this.state.transaction ? this.state.transaction : '...';
 
     return (
-      <div className="flex flex-col">
-        <div className="card">
-          <span className="card-title"> Would you like to buy or sell a coin? </span>
-          <div className="flex justify-around items-center mt-4">
-            <div
-              className={ this.state.transaction == 'buy' ? 'active-btn' : 'btn' }
-              onClick={ () => this.setState({ transaction: 'buy' }) }
-            >
-              Buying
+      <div>
+        {
+          this.state.isEditing ? (
+            <div className="flex flex-col">
+              <div className="card">
+                <span className="card-title"> Would you like to buy or sell a coin? </span>
+                <div className="flex justify-around items-center mt-4">
+                  <div
+                    className={ this.state.transaction == 'buy' ? 'active-btn' : 'btn' }
+                    onClick={ () => this.setState({ transaction: 'buy' }) }
+                  >
+                    Buying
+                  </div>
+                  <div
+                    className={ this.state.transaction == 'sell' ? 'active-btn' : 'btn' }
+                    onClick={ () => this.setState({ transaction: 'sell' }) }
+                  >
+                    Selling
+                  </div>
+                </div>
+              </div>
+              <div className="card-dark full">
+                <span>
+                  I have...
+                </span>
+                <div className="exchange-card" style={{ marginBottom: '2rem' }}>
+                  <select className="styled-input" placeholder="Coin">
+                    <option>Select a Coin</option>
+                    <option> WEEN </option>
+                    <option> AUTO </option>
+                    <option> MACHINE </option>
+                  </select>
+                  <input type="number" placeholder="Amount" className="styled-input" />
+                  <span className="conversion-totals">
+                    {`Market Value = $${this.state.baseConversionUSD} (${this.state.baseConversion})`}
+                  </span>
+                </div>
+                <div className="arrows-container">
+                  <div className="arrows-icon" />
+                </div>
+                <span className="mt-4">
+                  I want...
+                </span>
+                <div className="exchange-card">
+                  <select className="styled-input" placeholder="Coin">
+                    <option>Select a Coin</option>
+                    <option> WEEN </option>
+                    <option> AUTO </option>
+                    <option> MACHINE </option>
+                  </select>
+                  <input
+                    type="number"
+                    value={ this.state.exchangeAmount }
+                    className="styled-input"
+                    disabled
+                  />
+                  <span className="conversion-totals">
+                    {`Market Value = $${this.state.exchangeConversionUSD} (${this.state.exchangeConversion})`}
+                  </span>
+                </div>
+              </div>
+              <div
+                className="active-btn flex justify-center m-8"
+                onClick={ () => this.setState({ isEditing: false }) }
+              >
+                <span> Finish </span>
+              </div>
             </div>
-            <div
-              className={ this.state.transaction == 'sell' ? 'active-btn' : 'btn' }
-              onClick={ () => this.setState({ transaction: 'sell' }) }
+          ) : (
+          <div className="buy-sell-header">
+            <span> BuyOrSell 234 XRP with BTC </span>
+            <span
+              className="btn-simple"
+              onClick={ () => this.setState({ isEditing: true }) }
             >
-              Selling
-            </div>
+              edit
+            </span>
           </div>
-        </div>
-        <div className="card-dark full">
-          <span className="mb-8">
-            {`Which coin would you like to ${ buyOrSell  }?`}
-          </span>
-          <span>
-            I have...
-          </span>
-          <div className="exchange-card">
-            <select className="styled-input" placeholder="Coin">
-              <option>Select a Coin</option>
-              <option> WEEN </option>
-              <option> AUTO </option>
-              <option> MACHINE </option>
-            </select>
-            <input type="number" placeholder="Amount" className="styled-input" />
-          </div>
-          <div className="arrows-container">
-            <div className="arrows-icon" />
-          </div>
-          <span className="m-4">
-            I want...
-          </span>
-          <div className="exchange-card">
-            <select className="styled-input" placeholder="Coin">
-              <option>Select a Coin</option>
-              <option> WEEN </option>
-              <option> AUTO </option>
-              <option> MACHINE </option>
-            </select>
-            <input
-              type="number"
-              value={ this.state.exchangeAmount }
-              className="styled-input"
-            />
-          </div>
-        </div>
-        <div className="active-btn flex justify-center m-4">
-          <span> Finish </span>
-        </div>
+        )}
       </div>
     );
   }
